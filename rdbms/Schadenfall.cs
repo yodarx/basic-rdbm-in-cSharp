af05317e56d22f5c2f24ac9ad8f7952d7e804b0e
Jeremy Singh <jeremy.singh@edu.teko.ch>
2021-08-23 20:49:30 +0200

    feat(): intial commit, adding Schadenfall CLass

---
+++ rdbms/Schadenfall.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public enum State
+{
+    initialized,
+    inProgress,
+    auditied,
+    archived,
+    done,
+}
+
+
+public class Schadenfall
+{
+    int _claimNummer;
+    int _insurance;
+    int _insurant;
+    String state;
+    String description;
+
+    public Schadenfall()
+    {
+        this._claimNummer = _claimNummer;
+        this.state = State.initialized;
+        this._insurance = _insurance;
+        this._insurant = _insurant;
+        this.description = description;
+    }
+
+    public void save(Schadenfall) { 
+        //TODO save schadenfall to our file structure
+    }
+
+    public void change(State state) {
+        this.state = state;
+    }
+
+    public void archive(State state)
+    {
+        this.state = State.archived;
+        //TODO do more with archived stuff?
+    }
+
+    public void export(Schadenfall)
+    {
+        //TODO export a schadenfall to xml
+    }
+}
